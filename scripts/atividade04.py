c56696eaab261f6d7330959d32eeb63fa36ffc38
AnaClaraLF <47868749+AnaClaraLF@users.noreply.github.com>
2023-11-20 18:42:15 -0300

    atividade04
    atividade04

--- scripts/atividade04.py
+++ scripts/atividade04.py
@@ -13,15 +13,17 @@ dd = 31
 ## dados de -entrada-
 
 # pacotes
-import os
-import dask
+#import os
+#import dask
 import numpy as np
 import numpy.ma as ma
-import math
+#import math
 import matplotlib.pyplot as plt
 import netCDF4 as nc
 import xarray as xr
 import pandas as pd
+from statsmodels.tsa.seasonal import seasonal_decompose
+import pymannkendall as mk
 
 # # abrir arquivos, juntar e salvar novo arq
 # dask.config.set({"array.slicing.split_large_chunks": False})
@@ -43,8 +45,11 @@ import pandas as pd
 # Evap_tavg (April){kg/m2/sec} * 10800{sec/3hr} * 8{3hr/day} * 30{days}
 
 # conferir
-data = nc.Dataset('D:/ENS410064/Dados/brutos/earth/GLDAS_NOAH025_MASUB.nc4')
-data1 = nc.Dataset(r"D:\ENS410064\Dados\brutos\earth\GLDAS2\GLDAS_NOAH025_M.A202204.021.nc4.SUB.nc4")
+#data = nc.Dataset('D:/ENS410064/Dados/brutos/earth/GLDAS_NOAH025_MASUB.nc4')
+data = nc.Dataset(r'C:\Users\anafranco\Documents\ENS410064\GLDAS_NOAH025_MASUB.nc4')
+#data1 = nc.Dataset(r"D:\ENS410064\Dados\brutos\earth\GLDAS2\GLDAS_NOAH025_M.A202204.021.nc4.SUB.nc4")
+
+#data2 = data.subset(time=[0,283]) # descobrir como fazer subsets
 
 # extraindo dados 
 lon = data['lon'][:]
@@ -114,11 +119,18 @@ plt.figtext(0.75,0.5, df_tmp.describe().loc[['mean','std']].to_string())
 fig9,ax9 = plt.subplots()
 ax9.plot(evapCum[:,yi,xi])
 
+# verificar se há tendencia e sazonalidade
+components = seasonal_decompose(evapCum[:,yi,xi], model='aditive', period=12) 
+components.plot()
+plt.show()
+# mann-kendall
+result = mk.original_test(evapCum[:,yi,xi])
+print(result)
 
 ## Variabilidade espacial (TODOS PIXELS)
-# MEDIAS
-fig,ax = plt.subplots()
-ax.pcolor(lon,lat,np.mean(evap[:, :, :], axis = 0))
+# MEDIAS 
+fig2,ax2 = plt.subplots()
+ax2.pcolor(lon,lat,np.mean(evapCum[:, :, :], axis = 0))
 
 fig2,ax2 = plt.subplots()
 ax2.pcolor(lon,lat,np.mean(alb[:, :, :], axis = 0))
@@ -130,3 +142,55 @@ fig4,ax4 = plt.subplots()
 ax4.pcolor(lon,lat,np.mean(tveg[:, :, :],axis = 0 ))
 
 
+## Variabilidade espacial (TODOS PIXELS)
+
+# por mes
+mes = ['janeiro','fevereiro','marco','abril', 'maio','junho','julho','agosto',
+       'setembro', 'outubro','novembro','dezembro']
+fig,ax = plt.subplots(3,4,sharex=True)
+ax = ax.flatten() # nao importa a ordem dos plots
+for i in range (0,12):  #nao deu certo o loop
+    im=ax[i].pcolor(lon,lat,np.mean(evapCum[i:283:12, :, :], axis = 0))
+    ax[i].title.set_text(mes[i])
+    #fig.colorbar(im, ax=ax[3]) # descobrir como manter a colorbar para todos meses
+    
+    
+# por estacao
+# de um jeito burro pq nao sei a sintax
+a=np.arange(start=11,stop=283,step=12)
+b=np.arange(start=0,stop=283,step=12)
+c=np.arange(start=1,stop=283,step=12)
+ver=np.sort(np.concatenate((a,b,c),axis=0))
+del a, b ,c
+a=np.arange(start=2,stop=283,step=12)
+b=np.arange(start=3,stop=283,step=12)
+c=np.arange(start=4,stop=283,step=12)
+out=np.sort(np.concatenate((a,b,c),axis=0))
+del a, b ,c
+a=np.arange(start=5,stop=283,step=12)
+b=np.arange(start=6,stop=283,step=12)
+c=np.arange(start=7,stop=283,step=12)
+inv=np.sort(np.concatenate((a,b,c),axis=0))
+del a, b ,c
+a=np.arange(start=8,stop=283,step=12)
+b=np.arange(start=9,stop=283,step=12)
+c=np.arange(start=10,stop=283,step=12)
+pri=np.sort(np.concatenate((a,b,c),axis=0))
+del a, b ,c
+estac = ['outono','inverno','primavera','verao']
+# out=evapCum.reshape(2,5)[::12].ravel()
+fig,ax = plt.subplots(2,2,sharex=True)
+ax = ax.flatten() # nao importa a ordem dos plots
+im1 = ax[0].pcolor(lon,lat,np.mean(evapCum[out, :, :], axis = 0))
+ax[0].title.set_text(estac[0])
+fig.colorbar(im1, ax=ax[0])
+im2 = ax[1].pcolor(lon,lat,np.mean(evapCum[inv, :, :], axis = 0))
+ax[1].title.set_text(estac[1])
+fig.colorbar(im2, ax=ax[1])
+im3 = ax[2].pcolor(lon,lat,np.mean(evapCum[pri, :, :], axis = 0))
+ax[2].title.set_text(estac[2])
+fig.colorbar(im3, ax=ax[2])
+im4 = ax[3].pcolor(lon,lat,np.mean(evapCum[ver, :, :], axis = 0))
+ax[3].title.set_text(estac[3])
+fig.colorbar(im4, ax=ax[3])
+# descobrir como manter escala
